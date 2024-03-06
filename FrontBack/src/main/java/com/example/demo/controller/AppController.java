18c485c0631ed8e0be78eb2aa3bc12d23a48f438
enrsto <'enrico.stocchetti@sanmarcoinformatica.it'>
2024-03-06 09:16:19 +0100

    UomoRoccia :: modifische squashate

--- FrontBack/src/main/java/com/example/demo/controller/AppController.java
+++ FrontBack/src/main/java/com/example/demo/controller/AppController.java
@@ -1,9 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -12,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -27,6 +23,9 @@ public class AppController {
 		cities.add("Roma");
 		cities.add("Milano");
 		cities.add("Napoli");
+		cities.add("Varese");
+		cities.add("Vicenza");
+		cities.add("Venezia");
 	}
 	
 	
@@ -37,7 +36,7 @@ public class AppController {
 		Predicate<String> filterCity = city -> city.equals("Roma") || city.equals("Milano");
 		var lista  = cities.stream().collect(Collectors.toList());
 
-		//var lista  = cities.stream().filter(filterCity).collect(Collectors.toList());
+	//	var lista  = cities.stream().filter(filterCity).collect(Collectors.toList());
 		System.out.println(lista);
 		return lista;
 	}	
