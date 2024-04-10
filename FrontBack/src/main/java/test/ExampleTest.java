2e31d33740dcad39df79573a04940cea647e1a54
enrsto <'enrico.stocchetti@sanmarcoinformatica.it'>
2024-04-10 09:52:00 +0200

    Master

---
+++ FrontBack/src/main/java/test/ExampleTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class ExampleTest {
+	
+	 public static void main(String[] args) {
+		 
+		  // Effettua una richiesta GET e ottiene la risposta
+	        Response response = RestAssured.get("http://localhost:8080/out");
+
+	        // Esempio di utilizzo della risposta
+	        int statusCode = response.getStatusCode();
+	        String responseBody = response.getBody().asString();
+
+	        System.out.println("Status code: " + statusCode);
+	        System.out.println("Response body: " + responseBody);
+	        if (statusCode == 200) {
+	        	System.out.println("TEST OK!");
+	        }
+		 
+	 }
+	
+	
+
+}
--- FrontBack/src/test/java/com/example/demo/FrontBackApplicationTests.java
+++ FrontBack/src/test/java/com/example/demo/FrontBackApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
